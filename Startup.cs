5d2dbaacbef2cf8c6e58bae611ada900cf124c5d
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-08-28 19:43:44 -0300

    Adicionadas as classes que controlam as conexões.

---
+++ Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(ChatComSignalR.Startup))]
+
+namespace ChatComSignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR<MyConnection>("/echo");
+        }
+    }
+}
