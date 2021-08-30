5abb9c2b26379803c3498392b854e784448a5e63
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-08-30 07:55:18 -0300

    Primeira parte concluída

---
+++ obj/Release/Package/PackageTmp/Scripts/script.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    var connection = $.connection("/echo");
+    connection.start().done(function () {
+
+        $("#chat").val("Conected\n");
+
+        $("#sendButton").click(function () {
+
+            connection.send($("#txtBox").val());
+
+            $("#txtBox").val("");
+        });
+
+    });
+
+    connection.received(function (data) {
+
+        $("#chat").val($("#chat").val() + data + "\n");
+    });
+
+});
