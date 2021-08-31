22690809d282297c481647c6a9bc60740b3b5b5b
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-08-31 00:40:21 -0300

    Implementados os métodos que fazem o controle de usuários que entram online e saem.

--- MyConnection.cs
+++ MyConnection.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -9,9 +10,25 @@ namespace ChatComSignalR
 {
     public class MyConnection : PersistentConnection
     {
+        private static int QuantidadeConexoes = 0;
         protected override Task OnReceived(IRequest request, string connectionId, string data)
         {
             return Connection.Broadcast(data);
+        }
+
+        protected override Task OnConnected(IRequest request, string connectionId)
+        {
+            Interlocked.Increment(ref QuantidadeConexoes);
+            Connection.Broadcast("Online agora: " + QuantidadeConexoes);
+            Connection.Send(connectionId, "Bem-vindo " + connectionId);
+            return base.OnConnected(request, connectionId);
+        }
+
+        protected override Task OnDisconnected(IRequest request, string connectionId, bool stopCalled)
+        {
+            Interlocked.Decrement(ref QuantidadeConexoes);
+            Connection.Broadcast("Online agora: " + QuantidadeConexoes);
+            return base.OnDisconnected(request, connectionId, stopCalled);
         }
     }
 }
