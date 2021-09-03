49032b23c0529aeabba3e9ccedefa402f42abff2
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-09-02 23:24:53 -0300

    Implementando conexões para grupos.

--- MyConnection.cs
+++ MyConnection.cs
@@ -13,22 +13,34 @@ namespace ChatComSignalR
         private static int QuantidadeConexoes = 0;
         protected override Task OnReceived(IRequest request, string connectionId, string data)
         {
-            return Connection.Broadcast(data);
+            int i;
+            if ((i = data.IndexOf(":")) > -1) 
+            {
+                var groupName = data.Substring(0, i);
+                var messageOrCommand = data.Substring(i + 1);
+            }
+
+            return base.OnReceived(request, connectionId, data);
         }
 
-        protected override Task OnConnected(IRequest request, string connectionId)
-        {
-            Interlocked.Increment(ref QuantidadeConexoes);
-            Connection.Broadcast("Online agora: " + QuantidadeConexoes);
-            Connection.Send(connectionId, "Bem-vindo " + connectionId);
-            return base.OnConnected(request, connectionId);
-        }
+        //protected override Task OnReceived(IRequest request, string connectionId, string data)
+        //{
+        //    return Connection.Broadcast(data);
+        //}
 
-        protected override Task OnDisconnected(IRequest request, string connectionId, bool stopCalled)
-        {
-            Interlocked.Decrement(ref QuantidadeConexoes);
-            Connection.Broadcast("Online agora: " + QuantidadeConexoes);
-            return base.OnDisconnected(request, connectionId, stopCalled);
-        }
+        //protected override Task OnConnected(IRequest request, string connectionId)
+        //{
+        //    Interlocked.Increment(ref QuantidadeConexoes);
+        //    Connection.Broadcast("Online agora: " + QuantidadeConexoes);
+        //    Connection.Send(connectionId, "Bem-vindo " + connectionId);
+        //    return base.OnConnected(request, connectionId);
+        //}
+
+        //protected override Task OnDisconnected(IRequest request, string connectionId, bool stopCalled)
+        //{
+        //    Interlocked.Decrement(ref QuantidadeConexoes);
+        //    Connection.Broadcast("Online agora: " + QuantidadeConexoes);
+        //    return base.OnDisconnected(request, connectionId, stopCalled);
+        //}
     }
 }
